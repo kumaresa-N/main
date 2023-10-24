229c77ba05581a3af2514ae0c888d7208ddac091
arunkumaresan <m.arunkumaresan@gmail.com>
2023-10-24 20:32:42 +0530

    normal commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Button, Popup } from 'semantic-ui-react';
+import img1 from "./img/allu2.png"
 
 function App() {
   return (
     <div className="App">
-
+        <Popup content={<img src={img1} width='400px' height='400px' alt='allu'/>} trigger={<Button icon='add' />} />
     </div>
   );
 }
